b358c9f2d5e6772bbe0f1b586ccd4da71d8c11bd
Flotschi01 <Florian.Lehmerhofer@gmail.com>
2025-03-17 09:56:15 +0100

    stand 17.03.25

---
+++ JSON/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+using System.Text.Json;
+
+Console.WriteLine("Hello, World!");
+string path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+string filename = System.IO.Path.Combine(path, "semester.json");
+/*List<string> items = new List<string>(){"Flotschi", "Maxi", "Bergi"};
+string json = JsonSerializer.Serialize(items);
+File.WriteAllText(filename, json);*/
+string json = System.IO.File.ReadAllText(filename);
+List<string>? jsonList = System.Text.Json.JsonSerializer.Deserialize<List<string>>(json);
+foreach (var jsonItem in jsonList)
+{
+    Console.WriteLine(jsonItem);
+}
