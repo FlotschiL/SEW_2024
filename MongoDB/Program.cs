5e33d3f25708646aebdf6d89bfcdb72ee746c791
Flotschi01 <F.Lehmerhofer@htlkrems.at>
2025-05-20 09:21:21 +0200

    added First access like in the pp

---
+++ MongoDB/Program.cs
@@ -0,0 +1,35 @@
+﻿
+using MongoDB.Bson;
+using Newtonsoft.Json;
+
+var manager = new MongoManager("mongodb://localhost:27017", "SEW", "test");
+Console.WriteLine("reset? press y");
+if (Console.ReadKey(true).Key == ConsoleKey.Y)
+{
+    manager.DeleteAllAsync();
+    await manager.CreateSampleData(
+        @"C:\Users\MainUserFlo\Downloads\first-names.txt",
+        @"C:\Users\MainUserFlo\Downloads\last-names.txt"
+    );
+}
+/*var people = await manager.GetAllAsync();
+foreach (var person in people)
+    Console.WriteLine(JsonConvert.SerializeObject(person, Formatting.Indented));*/
+Dictionary<string, object?> Person = new Dictionary<string, object?>(){
+{"Name", null},
+{"LastName", null},
+{"Age", null},
+{"Gender", null}
+
+};
+string? input;
+foreach (var prop in Person)
+{
+    do
+    {
+        Console.WriteLine($"Enter {prop.Key}:");
+        Person[prop.Key] = Console.ReadLine();
+    }
+    while(Person[prop.Key] == null);
+}
+await manager.InsertAsync(Person);
